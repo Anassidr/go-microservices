36bbfba62bb3a47bb0559eac4ea8315afe04990c
Anassidr <kamalanass96@gmail.com>
2023-04-05 10:55:42 +0200

    0504

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/anassidr/go-microservices
 
 go 1.20
+
+require golang.org/x/net v0.8.0
--- main.go
+++ main.go
@@ -4,15 +4,46 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/anassidr/go-microservices/handlers"
+	"golang.org/x/net/context"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
-	http.ListenAndServe(":9090", sm)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// Goroutine : execute function concurrently with other functions
+	// Goroutines are different from traditional OS threads because they are managed by the GO runtime. Makes them more light weight.
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc) //wait until the requests that are currently handled by the server are completed and then shut down
 }
