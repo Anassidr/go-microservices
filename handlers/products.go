fc6b6f58613bfe497f19c7a07d2593d641815bf2
Anassidr <kamalanass96@gmail.com>
2023-04-11 09:58:17 +0200

    1104

--- handlers/products.go
+++ handlers/products.go
@@ -81,9 +81,13 @@ func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
 				return
 			}
 
-			ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+			ctx := context.WithValue(r.Context(), KeyProduct{}, prod) //Withvalue method on the context object is used to add a key-value pair to the context object
 			req := r.WithContext(ctx)
 
-			next.ServeHTTP(rw, req)
+			next.ServeHTTP(rw, req) //call the next handler in the middleware
 		})
 }
+
+//request object goes from client to server, through the middleware functions that modify the request (context or headers)
+//response object  goes back, passing through the same chain of middlewares
+//we are effectively building a pipeline
