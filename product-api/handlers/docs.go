fa48bb9d0f5e3017df757f49a17291ed9f039581
Anassidr <kamalanass96@gmail.com>
2023-04-13 15:58:09 +0200

    1310

--- product-api/handlers/docs.go
+++ product-api/handlers/docs.go
@@ -67,7 +67,7 @@ type productParamsWrapper struct {
 	Body data.Product
 }
 
-// swagger:parameters listSingleProducts deleteProduct
+// swagger:parameters listSingleProduct deleteProduct
 type productIDParamsWrapper struct {
 	// The id of the product for which the operation relates
 	// in: path
