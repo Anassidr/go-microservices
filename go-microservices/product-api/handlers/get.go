e2d49f3ec74dfe82c7aa310db7eaee9c76d72ae0
Anassidr <kamalanass96@gmail.com>
2023-04-14 12:10:44 +0200

    test passed

--- go-microservices/product-api/handlers/get.go
+++ go-microservices/product-api/handlers/get.go
@@ -17,6 +17,8 @@ import (
 func (p *Products) ListAll(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("[DEBUG] get all records")
 
+	rw.Header().Add("Content-Type", "application/json")
+
 	prods := data.GetProducts()
 
 	err := data.ToJSON(prods, rw)
