ebf5d9bb01b68c41bc03af4eabb5e54e4d92dfa1
Anassidr <kamalanass96@gmail.com>
2023-04-16 10:46:16 +0200

    gzip

--- go-microservices/product-images/handlers/zip_middleware.go
+++ go-microservices/product-images/handlers/zip_middleware.go
@@ -6,6 +6,13 @@ import (
 	"strings"
 )
 
+// Trivia :
+// The gzip format and algorithm were initially developed by Jean-loup Gailly and Mark Adler in the 1990s
+// as an alternative to the older Unix compress utility.
+// The gzip format is based on the Deflate algorithm, which is a combination of LZ77 and Huffman coding.
+
+// empty container used to group together related functionality
+
 type GzipHandler struct {
 }
 
@@ -14,7 +21,7 @@ func (g *GzipHandler) GzipMiddleware(next http.Handler) http.Handler {
 		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			// create a gziped response
 			wrw := NewWrappedResponseWriter(rw)
-			wrw.Header().Set("Content-Encoding", "gzip")
+			wrw.Header().Set("Content-Encoding", "gzip") // tell the client this response needs to be decompressed
 		}
 	})
 }
@@ -25,7 +32,7 @@ type WrappedResponseWriter struct {
 }
 
 func NewWrappedResponseWriter(rw http.ResponseWriter) *WrappedResponseWriter {
-	gw := gzip.NewWriter(rw)
+	gw := gzip.NewWriter(rw) // returns a pointer to a new gzip.Writer
 	return &WrappedResponseWriter{rw: rw, gw: gw}
 }
 
