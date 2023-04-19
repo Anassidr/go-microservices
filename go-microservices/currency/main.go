848bbdf6d36063e43113e8e3628d52eeac9f76e0
Anassidr <kamalanass96@gmail.com>
2023-04-19 10:55:36 +0200

    currency server

--- go-microservices/currency/main.go
+++ go-microservices/currency/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/anassidr/go-microservices/currency/server"
 	"github.com/hashicorp/go-hclog"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -17,6 +18,8 @@ func main() {
 	cs := server.NewCurrency(log)
 
 	protos.RegisterCurrencyServer(gs, cs)
+
+	reflection.Register(gs) // disable in production
 
 	l, err := net.Listen("tcp", ":9092")
 	if err != nil {
--- go-microservices/currency/server/currency.go
+++ go-microservices/currency/server/currency.go
@@ -9,12 +9,13 @@ import (
 
 // Currency is a gRPC server, it implements the methods defined by the CurrencyServer interface
 type Currency struct {
+	protos.UnimplementedCurrencyServer
 	log hclog.Logger
 }
 
 // NewCurrency creates a new Currency server
-func NewCurrency(l hclog.Logger) Currency {
-	return Currency{l}
+func NewCurrency(l hclog.Logger) *Currency {
+	return &Currency{log: l}
 
 }
 
@@ -25,5 +26,3 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 	c.log.Info("Handle GetRate", "base", rr.GetBase(), "destination", rr.GetDestination())
 	return &protos.RateResponse{Rate: 0.5}, nil
 }
-
-func (c *Currency) mustEmbedUnimplementedCurrencyServer() {}
