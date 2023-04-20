7cc3ac3f508a17ed9d1af915207a3fc369cbe631
Anassidr <kamalanass96@gmail.com>
2023-04-20 14:17:04 +0200

    consume ECB

---
+++ currency/data/rates_test.go
@@ -0,0 +1,17 @@
+package data
+
+import (
+	"testing"
+
+	"fmt"
+
+	"github.com/hashicorp/go-hclog"
+)
+
+func TestNewRates(t *testing.T) {
+	tr, err := NewRates(hclog.Default())
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Sprintf("%#v", tr.rates)
+}
