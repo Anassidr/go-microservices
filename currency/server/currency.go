15cd2f34c607cb1819495852e79531744681e4b0
Anassidr <kamalanass96@gmail.com>
2023-04-25 11:06:46 +0200

    error handling in bidirectional stream

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -41,7 +41,15 @@ func (c *Currency) handleUpdates() {
 				if err != nil {
 					c.log.Error("Unable to get updated rate", "base", rr.GetBase().String(), "destination", rr.GetDestination().String())
 				}
-				err = k.Send(&protos.RateResponse{Base: rr.Base, Destination: rr.Destination, Rate: r})
+				err = k.Send(
+					&protos.StreamingRateResponse{
+						Message: &protos.StreamingRateResponse_RateResponse{
+							RateResponse: &protos.RateResponse{
+								Base: rr.Base, Destination: rr.Destination, Rate: r},
+						},
+					},
+				)
+
 				if err != nil {
 					c.log.Error("Unable to send updated rate", "base", rr.GetBase().String(), "destination", rr.GetDestination().String())
 
@@ -81,15 +89,21 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 	return &protos.RateResponse{Base: rr.Base, Destination: rr.Destination, Rate: rate}, nil
 }
 
+// SubscribeRates implements the gRPC bidirection streaming method for the server
+
 func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
 
+	// handle client message
 	for {
-		rr, err := src.Recv()
+		rr, err := src.Recv() // Recv is a blocking method that returns on client data
 
+		// io.EOF signals that the client closed the connection
 		if err == io.EOF {
 			c.log.Info("Client has closed connection")
 			break
 		}
+
+		// any other error means the transport btw the server and client is unavailable
 		if err != nil {
 			c.log.Error("Unable to read from client", "error", err)
 			return err
@@ -100,6 +114,38 @@ func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) erro
 		if !ok {
 			rrs = []*protos.RateRequest{}
 		}
+
+		// check that the subscription does not exist
+		var validationError *status.Status
+		for _, v := range rrs {
+			if v.Base == rr.Base && v.Destination == rr.Destination {
+				validationError = status.Newf(
+					codes.AlreadyExists,
+					"Unable to subscribe for currency as subscription already exists",
+				)
+				// add the original request as metadata
+				validationError, err = validationError.WithDetails(rr)
+				if err != nil {
+					c.log.Error("Unable to add metadata to error", "error", err)
+					break
+				}
+
+				break
+			}
+		}
+
+		// if a validation error return error and continue
+		if validationError != nil {
+			src.Send(
+				&protos.StreamingRateResponse{
+					Message: &protos.StreamingRateResponse_Error{
+						Error: validationError.Proto(),
+					},
+				},
+			)
+			continue
+		}
+		// all ok
 		rrs = append(rrs, rr)
 		c.subscriptions[src] = rrs
 	}
